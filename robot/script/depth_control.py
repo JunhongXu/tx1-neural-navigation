b2ca7da74dcaea78e833e381669b47363cbf11e7
jxu7 <jxu7@bsu.edu>
2017-04-08 16:00:57 -0400

    testing

--- robot/script/depth_control.py
+++ robot/script/depth_control.py
@@ -43,7 +43,7 @@ class DepthController(object):
         data = data[~np.isinf(data)]
         return data
 
-    def count(self, data, dist=0.9):
+    def count(self, data, dist=1.5):
         return data[data<=dist].shape[0]/data.shape[0]
 
     def update_depth(self, data):
