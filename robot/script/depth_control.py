979b536424e984e97a67a51c937a6fa35f792ea0
jxu7 <jxu7@bsu.edu>
2017-04-08 11:29:13 -0400

    testing

--- robot/script/depth_control.py
+++ robot/script/depth_control.py
@@ -73,14 +73,14 @@ class DepthController(object):
                 self.twist.linear.x = 0.0
                 print('[!]Stop')
 
-            if np.mean(info[0:2])/np.mean(info[-2:-1]) < 0.6:
+            if np.mean(info[0:2])/np.mean(info[-2:-1]) < 0.5:
                 self.twist.angular.z = 4.5 - 2 * self.sigmoid(info[0]/whole_mean)
                 self.twist.angular.z = -self.twist.angular.z
                 print('Turn right')
-            elif np.mean(info[-2:-1])/np.mean(info[0:2]) < 0.6:
+            elif np.mean(info[-2:-1])/np.mean(info[0:2]) < 0.5:
                 self.twist.angular.z = 4.5 - 2 * self.sigmoid(info[-1]/whole_mean)
                 print('Turn left')
-            elif np.any(info[2:4]/whole_mean<1.0):
+            elif np.any(info[2:4]/whole_mean<0.8):
                 # compare left and right
                 if info[0] > info[-1]:
                     self.twist.angular.z = 2.5
