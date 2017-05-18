1a1cf1eeb7400c42ee53844c784cca21b7aa412c
jxu7 <jxu7@bsu.edu>
2017-05-18 14:41:36 -0400

    added visualization

--- robot/script/depth_control.py
+++ robot/script/depth_control.py
@@ -63,6 +63,7 @@ class DepthController(object):
             center_win = self.reject_outliers(self.reject_nan_inf(depth_img[:, W//3:2*W//3]))
             right_win = self.reject_outliers(self.reject_nan_inf(depth_img[:, 2*W//3:]))
             self.twist.linear.x = 0.5
+            print(left_win.shape, center_win.shape, right_win.shape)
             if self.count(center_win) >= 0.3:
                 if np.sum(left_win) < np.sum(right_win):
                     self.twist.angular.z = -4.5*self.count(left_win)
